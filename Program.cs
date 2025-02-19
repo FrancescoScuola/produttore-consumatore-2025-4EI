4e0f0ef1752e937631e9b3c81e10727925e34bd2
Francesco Belloni <francobelloni85@libero.it>
2025-02-19 10:56:07 +0100

    [add] visualizzare risultati produttore consumatore

--- Program.cs
+++ Program.cs
@@ -12,14 +12,26 @@
 
         static void Main(string[] args)
         {            
-            Thread producer = new Thread(ProduceNumber);
-            Thread consumer = new Thread(ConsumeNumber);
+            Thread threadProducer = new Thread(ProduceNumber);
+            Thread threadConsumer = new Thread(ConsumeNumber);
 
-            producer.Start();
-            consumer.Start();
+            threadProducer.Start();
+            threadConsumer.Start();
 
-            producer.Join();
-            consumer.Join();
+            threadConsumer.Join();
+            threadProducer.Join();
+           
+
+            Console.WriteLine("il totale dei numeri prodotti è: " + producer.GetCount());
+            Console.WriteLine("il totale dei numeri prodotti è: " + consumer.GetCountNumberGenerator());
+
+            Console.WriteLine("il totale dei numeri consumati è: " + consumer.GetCount());
+            Console.WriteLine("il totale dei numeri consumati è: " + consumer.GetCountNumberGenerator());
+
+            var result = lQueue.GetCount();
+            const string message = "il totale dei numeri presenti nella coda è: ";
+            Console.WriteLine(message + result.Count);
+            Console.WriteLine("il totale della coda è: " + result.CountNumberGenerator);
 
             Console.ReadKey();
         }
@@ -27,26 +39,33 @@
         public static void ProduceNumber()
         {
             DateTime start = DateTime.Now;
-            while (DateTime.Now < start.AddSeconds(10))
+            while (DateTime.Now < start.AddSeconds(5))
             {
                 if ((lQueue.Count) < 6)
                 {
-                    
+
                     lQueue.Enqueue(producer.GiveMeNumber());
-                    
+
+                }
+                else {
+                    Console.WriteLine("il produttore sta consumando CPU");
                 }
             }
+            
             Console.WriteLine("ho fatto 10 secondi e finito di produrre numeri");
         }
 
         public static void ConsumeNumber()
         {
             DateTime start = DateTime.Now;
-            while (DateTime.Now < start.AddSeconds(10))
+            while (DateTime.Now < start.AddSeconds(5))
             {
                 if ((lQueue.Count) > 0)
                 {
                     consumer.ConsumeNumber(lQueue.Dequeue());
+                }
+                else { 
+                    Console.WriteLine("il consumatore sta consumando CPU");
                 }
             }
             Console.WriteLine("ho fatto 10 secondi e finito di consumare dati");
@@ -58,34 +77,74 @@
     class Producer
     {
         private Random _random = new Random(772);
+        int _count = 0;
+        int _countNumberGenerator = 0;
         public int GiveMeNumber()
         {
             int i = _random.Next(100);
             Console.WriteLine("ho prodotto il numero: " + i);
+            _count = _count +i;
+            _countNumberGenerator++;
             return i;
+        }
+        public int GetCount()
+        {
+            return _count;
+        }
+        public int GetCountNumberGenerator()
+        {
+            return _countNumberGenerator;
         }
     }
 
     class Consumer
     {
         private List<int> _numbers = new List<int>();
+        int _countConsumer = 0;
+        int _countNumberConsumer = 0;
         public void ConsumeNumber(int number)
         {
             _numbers.Add(number);
             Console.WriteLine("ho consumato il numero: " + number);
+            _countConsumer += number;
+            _countNumberConsumer++;
+        }
+        public int GetCount()
+        {
+            return _countConsumer;
+        }
+        public int GetCountNumberGenerator()
+        {
+            return _countNumberConsumer;
         }
     }
-
-    class LimitedQueue<T> : Queue<T>
+    class LimitedQueueCountResult
+    {
+        public int Count { get; set; }
+        public int CountNumberGenerator { get; set; }
+        
+    }   
+    class LimitedQueue<T> : Queue<int>
     {
         private int _maxSize;
-
+        
+        public LimitedQueueCountResult GetCount()
+        {
+            LimitedQueueCountResult result = new LimitedQueueCountResult();
+            while (this.Count != 0) {                
+                var isSuccess = this.TryDequeue(out int t);
+                // var t = this.Dequeue();
+                result.CountNumberGenerator++;
+                result.Count += t;
+            }
+            return result;
+        }
         public LimitedQueue(int maxSize)
         {
             _maxSize = maxSize;
         }
 
-        public new void Enqueue(T item)
+        public new void Enqueue(int item)
         {
             if (this.Count >= _maxSize)
             {
